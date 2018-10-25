7e0c9d44022cf263cdb0d2b17cb871d7ed78963b
rossanaf <38791109+rossanaf@users.noreply.github.com>
2018-10-25 16:46:06 +0100

    Add files via upload

--- html/guest.php
+++ html/guest.php
@@ -136,6 +136,34 @@
 		</div>
 	</div>
 </div>
+<div class='container'>
+	<p id="timer" style="font-size:30px"></p>
+</div>
 <?php	
 	include($_SERVER['DOCUMENT_ROOT']."/html/footer.php"); 
 ?>
+
+<script>
+	// Set the date we're counting down to
+	var countDownDate = new Date("Jan 5, 2019 15:37:25").getTime();
+	// Update the count down every 1 second
+	var x = setInterval(function() {
+	  // Get todays date and time
+	  var now = new Date().getTime();
+	  // Find the distance between now and the count down date
+	  var distance = countDownDate - now;
+	  // Time calculations for days, hours, minutes and seconds
+	  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+	  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+	  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+	  // Display the result in the element with id="timer"
+	  document.getElementById("timer").innerHTML = days + "d " + hours + "h "
+	  + minutes + "m " + seconds + "s ";
+	  // If the count down is finished, write some text 
+	  if (distance < 0) {
+	    clearInterval(x);
+	    document.getElementById("timer").innerHTML = "EXPIRED";
+		  }
+	}, 1000);
+</script>
